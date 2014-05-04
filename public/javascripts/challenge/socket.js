7bd5abe65d83052e226f2ac2d5b4c25f8734c359
Daniel Wang <danielwpz@gmail.com>
2014-05-04 16:45:25 +0800

    Fix missing symbols in previous version.

--- public/javascripts/challenge/socket.js
+++ public/javascripts/challenge/socket.js
@@ -1,4 +1,15 @@
 function registerListeners(so) {
+	so.on('_Reply', function(data) {
+		if (data.type == 'Redir') {
+			var path = data.page;
+			var hostPath = window.location.hostname;
+			var hostPort = window.location.port;
+			// do redirection
+			window.location.assign('http://' + hostPath + ':' +
+				hostPort + path);
+		}
+	});
+
 	so.on('_Start', function(data) {
 		console.log('start!');
 	});
@@ -6,6 +17,10 @@ function registerListeners(so) {
 	so.on('_Update', function(data) {
 		console.log('Update: ' + data);
 		doUpdate(data);
+	});
+}
+
+function doUpdate(data) {
 }
 
 function sendUpdate(data) {
--- routes/typing.js
+++ routes/typing.js
@@ -5,13 +5,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-	levelManager({levelPath: './levels/'}, function(err, data) {
-		res.render('typing', {
-			title: 'Challenge',
-			layout: 'challenge_layout',
-			code: data
+	if (req.session && req.session.pairIndex >= 0) {
+		levelManager({levelPath: './levels/'}, function(err, data) {
+			res.render('typing', {
+				title: 'Challenge',
+				layout: 'challenge_layout',
+				code: data
+			});
 		});
-	});
+	}else {
+		res.end('You should register or challenge first.');
+	}
 });
 
 module.exports = router;
